afc69704774effd775a08293a31d693995386707
michelle <michelletyson29@gmail.com>
2018-12-29 22:50:28 -0800

    started the pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// create the variables
+
+// create the function to generate a random number
+
+// create an onclick event that will recognize the number the user enters
+
+// compare the user entered number to the computer generated number
+
+//if the numbers are equal to eachother, add one point to wins
+
+// if the numbers are not equal, add a point to loses
+
+//update the number of guesses left
+
+//display and update the numbers already guessed
