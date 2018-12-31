1e8a0b82caaa812d418c32496475670df34f5486
michelle <michelletyson29@gmail.com>
2018-12-31 14:39:08 -0800

    added onkeyup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,18 +8,25 @@ var guessesSoFar = [];
 var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // have the computer generate a random letter
-var compGues = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+var compGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
 
 
 // create an onclick event that will recognize the letter the user enters
-//convert the letter to lowercase, to make sure it matches the computer letter
 
-// compare the user entered letters to the computer generated letters
+document.onkeyup = function(event) {
 
-//if the letters are equal to eachother, add one point to wins
 
-// if the letters are not equal, add a point to loses
 
-//update the number of guesses left
+    //convert the letter to lowercase, to make sure it matches the computerOptions array
+    var computerOptions = event.key.toLowerCase();
 
-//display and update the letters already guessed
+    // compare the user entered letters to the computer generated letters
+
+    //if the letters are equal to eachother, add one point to wins
+    
+    // if the letters are not equal, add a point to loses
+
+    //update the number of guesses left
+
+    //display and update the letters already guessed
+};
