938ba08c096207fa2ab6b4d8fc08ec44f6568ac3
michelle <michelletyson29@gmail.com>
2019-01-01 22:21:39 -0800

    added an array to store the computer guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ var wins = 0;
 var loses = 0;
 var guessesLeft = 10;
 var guessesSoFar = [];
+var computerGuess = [];
 
 //make an array with all letters available to guess
 var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
