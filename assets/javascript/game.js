dfd9019677e8bc5ed2dd9151b0910ba7565671af
michelle <michelletyson29@gmail.com>
2019-01-04 10:05:58 -0800

    moved compGuess out of the function; added push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,25 +8,36 @@ var wins = 0,
 //make an array with all letters available to guess
 var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// have the computer generate and store a random letter
+var compGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+//add the letter to the array
+computerGuess.push(compGuess);
+
 document.onkeyup = function(event) {
     // create an onclick event that will recognize the letter the user enters
     //create a variable to store the user entered letter
     var userGuess = event.key;
 
-    // have the computer generate and store a random letter
-    var compGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-
     // compare the user entered letters to the computer generated letters
     //if the letters are equal to eachother, add one point to wins
-    if (userGuess=== computerGuess){
+    if (userGuess === computerGuess){
         wins++;
-    }
-    // if the letters are not equal, add a point to loses
-    else {
-        losses++;
+    
     }
     
-    //update the number of guesses left
+    
+    //if the letters are not equal
+    else {
+        //add the user guessed letter to the array
+        guessesSoFar.push(userGuess);
+        guessesLeft--;
+
+        // if the letters are not equal after 10 guesses, add a point to loses
+        //update the number of guesses left
+        if (guessesLeft == 0) {
+            losses++;
+            
+    }
 
     //display and update the letters already guessed
 };
