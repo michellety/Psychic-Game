20cb583b5c09e44ca7337f9cffd8726853dd870f
michelle <michelletyson29@gmail.com>
2019-01-03 16:50:35 -0800

    moved variables into function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,26 @@
 // create the variables
-var wins = 0;
-var loses = 0;
-var guessesLeft = 10;
-var guessesSoFar = [];
-var computerGuess = [];
+var wins = 0,
+    loses = 0,
+    guessesLeft = 10,
+    guessesSoFar = [],
+    computerGuess = [];
 
 //make an array with all letters available to guess
 var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// have the computer generate a random letter
-var compGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+
+//push the generated letter into the computerGuess array
+computerGuess.push(compGuess);
 
 
-// create an onclick event that will recognize the letter the user enters
 
 document.onkeyup = function(event) {
+    // have the computer generate a random letter
+    var compGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
 
-
-
-    //convert the letter to lowercase, to make sure it matches the computerOptions array
-    var computerOptions = event.key.toLowerCase();
-
+    // create an onclick event that will recognize the letter the user enters
+    //create a variable to store the user entered letter
+    var userGuess = event.key;
     // compare the user entered letters to the computer generated letters
 
     //if the letters are equal to eachother, add one point to wins
