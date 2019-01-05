e473c4cb673018d54744f095dadfe1141baf710d
michelle <michelletyson29@gmail.com>
2019-01-05 13:09:30 -0800

    changed the variabler computerGuess from an array. updated resets

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,20 +3,25 @@ var wins = 0,
     losses = 0,
     guessesLeft = 10,
     guessesSoFar = [],
-    computerGuess = [];
+    computerGuess;
 
 //make an array with all letters available to guess
 var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // have the computer generate and store a random letter
 var compGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-//add the letter to the array
-computerGuess.push(compGuess);
+//set the random letter to the variable computerGuess
+computerGuess = compGuess;
+//test that the computer stored a letter
+console.log(computerGuess);
 
 document.onkeyup = function(event) {
     // create an onclick event that will recognize the letter the user enters
     //create a variable to store the user entered letter
     var userGuess = event.key;
+
+    //test
+    console.log(userGuess);
 
     // compare the user entered letters to the computer generated letters
     //if the letters are equal to eachother, add one point to wins
@@ -25,9 +30,11 @@ document.onkeyup = function(event) {
         //reset the game to play again
         guessesLeft = 10;
         guessesSoFar = [];
-        //computer chooses a new number
-        compGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-    
+        //computer chooses and stores a new random letter
+        var compGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+        computerGuess = compGuess;
+        //test 
+        console.log(computerGuess);
     }
     
     //if the letters are not equal
@@ -44,9 +51,11 @@ document.onkeyup = function(event) {
             //reset the game to play again
             guessesLeft = 10;
             guessesSoFar = [];
-            //computer chooses a new number
-            compGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-            
+            // have the computer generate and store a random letter
+            var compGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+            computerGuess = compGuess;
+            //test 
+            console.log(computerGuess);
         }
     }
   
